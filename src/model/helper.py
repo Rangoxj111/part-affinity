f3e9b08921558703b690dd8cc013b01721d10c0a
Rango_go <wangong1992@163.com>
2019-04-30 16:05:49 +0800

    Update helper.py
    add the bias of init

--- src/model/helper.py
+++ src/model/helper.py
@@ -5,10 +5,13 @@ import math
 def init(model):
     for m in model.modules():
         if isinstance(m, nn.Conv2d):
-            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-            m.weight.data.normal_(0, math.sqrt(2. / n))
+            nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
+            m.bias.data.zero_()
         elif isinstance(m, nn.BatchNorm2d):
             m.weight.data.fill_(1)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.linear):
+            m.weight.data.normal_(0,0.01)
             m.bias.data.zero_()
 
 
